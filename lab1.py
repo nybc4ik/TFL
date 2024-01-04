4bbc19b0246fdf6130cde8f242625554fb6faa44
nybc4ik <kamaevsergei1@gmail.com>
2024-01-04 17:31:25 +0300

    Исправил баги и поправил вывод, теперь всё работает исправно... я на это надеюсь

--- lab1.py
+++ lab1.py
@@ -3,6 +3,7 @@ import sys
 
 
 def rewriting(terms):
+    global counter_new
     term1 = terms[:terms.find("=")]
     term2 = terms[terms.find("=")+1:]
     #print(term1, term2)
@@ -20,14 +21,13 @@ def rewriting(terms):
             else:
                 if str1 == ")":
                     counter -= 1
-    #print(counter)   
-    if counter !=0:
+    #print(constr, "\n", counter) 
+    if counter != 0:
         print("не хватает скобки!") 
         sys.exit()
     #else:
         #print("всё ок :-)")
 
-    counter = 0 
     for i in range(len(term2)):
         str1 = term2[i]
         if str1 != "(" and str1 != ")" and str1 != ',':
@@ -39,8 +39,8 @@ def rewriting(terms):
             else:
                 if str1 == ")":
                     counter -= 1
-    #print(counter)   
-    if counter !=0:
+    #print(constr, "\n", counter)  
+    if counter != 0:
         print("не хватает скобки!") 
         sys.exit()
     #else:
@@ -48,22 +48,29 @@ def rewriting(terms):
 
     # подсчёт коэффициентов
     
-    counter_new = 0
     str2 = "And( "
     for i in constr:
-        str0 = "And( "
+        str0 = "And("
         str3 = ""
         counter_right = 0 
         counter_left = 0 
-        counter=0
+        counter = 1
+        check1 = False
         for j in terms:
-            if j == '(':
-                counter_left += 1
-            elif j == ')':
-                counter_right += 1
-            elif counter_right == counter_left:
-                counter +=1
+            if j == i:
+                check1 = True
+            if check1:
+                if j == '(':
+                    counter_left += 1
+                if j == ')':
+                    counter_right += 1
+                if  counter_left - counter_right == 1 and j== ',':
+                    counter += 1
+                if  counter_left == counter_right and counter_left != 0:
+                    counter +=1
+                    break
         #print("проверка подсчёта ",counter)    
+                
         all_counter = []
         all_counter.append(counter)
         arr = []
@@ -71,19 +78,19 @@ def rewriting(terms):
             abc = str(counter_new)
             str1 = "a" + abc
             arr.append(str1)
-            counter_new +=1
-            if k != counter - 1:
-                str0 += str1 + ">1"
+            counter_new += 1
+            if k == counter - 1:
+                str3 = str1 + ">0 "
             else:
-                str3 = str1 + ">0"
-        if len(str3) > 4:
-            str3 = str3[:-2] + "), "
+                str0 += str1 + ">1, "
+        if len(str0) > 4:
+            str0 = str0[:-2] + "), "
         else:
-            str3 =""
+            str0 = ""
         str2 += "Or( " + str0 + str3 + "), "
         print ("Вот так вот: ",str2)
         coefficients[i] = arr
-    str2 = str2[:-2]+")"
+    str2 = str2[:-2] +")"
 
     print('coefficients ', coefficients)
     print('test ', arr)
@@ -107,6 +114,7 @@ variables =  ''.join(variables.split())
 variables = variables[variables.find("=")+1:]
 variables = variables.split(",")
 
+counter_new = 0
 all_counter = []
 terms = []
 constr = []
